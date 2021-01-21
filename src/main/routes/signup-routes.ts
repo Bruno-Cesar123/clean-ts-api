4e7c18fb245b21183641d0edb2a494c3ee45663a
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-21 16:44:42 -0300

    feat: ensure mongodb reconnect is client is down

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup'
 import { adaptRoute } from '../adapters/express-routes-adapter'
+import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
+
+/* router.post('/signup', (req, res) => {
+    res.json({ ok: 'ok' })
+  }) */
