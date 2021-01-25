718649a68ad8cb255f18256c88a545b2c36457d7
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-25 16:49:47 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -16,18 +16,24 @@ export class LoginController implements Controller {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
+    const requiredFiels = ['email', 'password']
+    for (const field of requiredFiels) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+        return badRequest(new MissingParamError('email'))
       }
       if (!password) {
-        return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+        return badRequest(new MissingParamError('password'))
       }
       const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) {
-        return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+        return badRequest(new InvalidParamError('email'))
       }
       await this.authentication.auth(email, password)
     } catch (error) {
