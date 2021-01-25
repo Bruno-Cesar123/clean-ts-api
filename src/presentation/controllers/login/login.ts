5c95098a1efbb16f12985f1071ec8f927e358cc8
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-25 15:01:06 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
     if (!httpRequest.body.email) {
@@ -11,5 +18,6 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid(httpRequest.body.email)
   }
 }
