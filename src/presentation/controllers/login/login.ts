cfb9c9c0a20e2e25f9e53fdae159fda54a0fb734
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-26 20:41:42 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,39 +1,25 @@
-import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
-import { Controller, EmailValidator, HttpRequest, HttpResponse, Authentication } from './login-protocols'
+import { Controller, HttpRequest, HttpResponse, Authentication, Validation } from './login-protocols'
 
 export class LoginController implements Controller {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
-  private readonly emailValidator: EmailValidator
+  private readonly validation: Validation
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
   private readonly authentication: Authentication
 
-  constructor (emailValidator: EmailValidator, authentication: Authentication) {
-    this.emailValidator = emailValidator
+  constructor (authentication: Authentication, validation: Validation) {
+    this.validation = validation
     this.authentication = authentication
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
-    const requiredFiels = ['email', 'password']
-    for (const field of requiredFiels) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
-      }
-    }
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { email, password } = httpRequest.body
-      if (!email) {
-        return badRequest(new MissingParamError('email'))
-      }
-      if (!password) {
-        return badRequest(new MissingParamError('password'))
-      }
-      const isValid = this.emailValidator.isValid(email)
-
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
       const accessToken = await this.authentication.auth(email, password)
       if (!accessToken) {
         return unauthorized()
