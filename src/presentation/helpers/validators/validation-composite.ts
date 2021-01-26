f603ab7f4fb370e7108d3ba3519f0474ccbf448b
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-26 10:47:49 -0300

    refactor: move required field validation to the ValidationComposite

---
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -0,0 +1,20 @@
+import { Validation } from './validation'
+
+export class ValidationComposite implements Validation {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+  private readonly validations: Validation[]
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+  constructor (validations: Validation[]) {
+    this.validations = validations
+  }
+
+  validate (input: any): Error | any {
+    for (const validation of this.validations) {
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
+    }
+  }
+}
