65dc43206ed70b51aa34d93a9db17bd40741869d
Bruno Cesar <brunocesar12386@gmail.com>
2021-01-28 13:02:24 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,8 +1,8 @@
+import { LogErrorRepository } from '../../data/protocols/db/log-error-reposity'
+import { AccountModel } from '../../domain/models/account'
+import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
-import { serverError, ok } from '../../presentation/helpers/http/http-helper'
-import { LogErrorRepository } from '../../data/protocols/log-error-reposity'
-import { AccountModel } from '../../domain/models/account'
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
